62b652186ca6791aa6c76bd791cfb0753b8c2885
VladislavBash <vobashara.miem.hse.ru>
2022-06-12 17:46:02 +0300

    Updated higher_point function

--- Monte-Carlo/Monte-Carlo.hpp
+++ Monte-Carlo/Monte-Carlo.hpp
@@ -1,7 +1,7 @@
 #include <math.h>
 #include <random>
 #include <iostream>
-#include "../fparser.hh"
+#include "../parser/fparser.hh"
 
 double f(double x, std::string function) {
     // return pow(x, 2); // функция, например y = x^2
@@ -18,7 +18,7 @@ double random(double a, double b) {
     return distribution(mt);
 }
 
-double inter(double a, double b, double bordy, int n, std::string function) {
+double intergral(double a, double b, double bordy, int n, std::string function) {
     int count = 0; // счётчик точек под графиком функции
     for (int i = 0; i < n; i++) {
         // srand((unsigned int)time(NULL));
@@ -34,17 +34,19 @@ double inter(double a, double b, double bordy, int n, std::string function) {
     }
     return ((double)count*(double)(b-a)*(double)bordy)/(double)n;
 }
-double der(double a, double b, double e, std::string function) {
+double higher_point(double a, double b, double e, std::string function) {
     e = 0.001;
     double v[2] = {0, 0};
     double h = 0;
-    for (double i = a; i < b-e; i += e) {
+    double g = 0;
+    for (double i = a; i < b; i += e) {
         v[0] = v[1];
         double u = f(i, function);
-        v[1] = (u-f(i+e, function))/e;
+        g = std::max(g, u);
+        v[1] = (f(i+e, function)-u)/e;
         if (v[1] <= 0 && v[0] > 0){
             h = std::max(h, u);
         }
     }
-    return h;
+    return std::max(g, h);
 }

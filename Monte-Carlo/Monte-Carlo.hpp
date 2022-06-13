5a52b8136fae06467136fda905b5f19e0946ceec
VladislavBash <vobashara.miem.hse.ru>
2022-06-13 13:22:04 +0300

    added error estimate and integraion in polar crds

--- Monte-Carlo/Monte-Carlo.hpp
+++ Monte-Carlo/Monte-Carlo.hpp
@@ -1,14 +1,14 @@
 #include <math.h>
 #include <random>
+#include <stdlib.h>
 #include <iostream>
 #include "../parser/fparser.hh"
+#define PI 3.14
 
 double f(double x, std::string function) {
-    // return pow(x, 2); // функция, например y = x^2
     FunctionParser fparser;
     fparser.Parse(function, "x");
     return fparser.Eval(&x);
-    // return pow(x, 2);
 }
 
 double random(double a, double b) {
@@ -18,35 +18,58 @@ double random(double a, double b) {
     return distribution(mt);
 }
 
-double intergral(double a, double b, double bordy, int n, std::string function) {
+double intergral(double a, double b, double border, int num, std::string function, int choice) {
     int count = 0; // счётчик точек под графиком функции
-    for (int i = 0; i < n; i++) {
-        // srand((unsigned int)time(NULL));
-        // srand((unsigned int)(time(NULL))+i);
-        // double x = (((double)rand()/(double)(RAND_MAX)) * (b - a)) + a;
-        // srand((unsigned int)(time(NULL))+i+b+a+bordY+rand());
-        // double y = ((double)rand()/(double)(RAND_MAX)) * bordY;
+    if (choice == 2) {
+        function = "(1/2)*(" + function + ")^2";
+    }
+    for (int i = 0; i < num; i++) {
         double x = random(a, b);
-        double y = random(0, bordy);
+        double y = random(0, border);
         if (f(x, function) > y) {
             count++;
         }
     }
-    return ((double)count*(double)(b-a)*(double)bordy)/(double)n;
+    return ((double)count*(double)(b-a)*(double)border)/(double)num;
 }
-double higher_point(double a, double b, double e, std::string function) {
+
+double higher_point(double a, double b, double e, std::string function, int choice) {
     e = 0.001;
-    double v[2] = {0, 0};
-    double h = 0;
+    // double v[2] = {0, 0};
+    // double h = 0;
     double g = 0;
-    for (double i = a; i < b; i += e) {
-        v[0] = v[1];
-        double u = f(i, function);
-        g = std::max(g, u);
-        v[1] = (f(i+e, function)-u)/e;
-        if (v[1] <= 0 && v[0] > 0){
-            h = std::max(h, u);
-        }
+    if (choice == 2) {
+        function = "(1/2)*(" + function + ")^2";
     }
-    return std::max(g, h);
+    for (double i = a; i < b; i += e) {
+        // v[0] = v[1];
+        // double u = f(i, function);
+        g = std::max(g, f(i, function));
+        // v[1] = (f(i+e, function)-u)/e;
+        // if (v[1] <= 0 && v[0] > 0){
+        //     h = std::max(h, u);
+        // }
+    }
+    return g;
+}
+
+double truth_value(double * S, int num) {
+    double result = 0;
+    for (int i = 0; i < num; i++) {
+        result += S[i];
+    }
+    return result / num;
+}
+
+double summ_of_delta_x(double * S, int num) {
+    double result = 0;
+    double truth_x = truth_value(S, num);
+    for (int i = 0; i < num; i++) {
+        result = pow((S[i]-truth_x), 2);
+    }
+    return result;
+}
+
+double accuracy(double * S, int num) { // Оценка погрешности
+    return sqrt(summ_of_delta_x(S, num)/((num-1)*num));
 }

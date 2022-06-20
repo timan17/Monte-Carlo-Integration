c8554cb47fb3135486e2c616e24c25fe99febdf7
VladislavBash <vobashara.miem.hse.ru>
2022-06-20 17:39:43 +0300

    added several colours

--- Monte-Carlo/Draw.h
+++ Monte-Carlo/Draw.h
@@ -1,14 +1,36 @@
 #ifndef MONTE_CARLO_DRAW_H
 #define MONTE_CARLO_DRAW_H
+#define AXIS '#92088a'
 
 #include "Monte-Carlo.hpp"
 #include <map>
 #include <string>
 #include "gnuplot.h"
 
-void Draw(int num, double a, double b, double e, double border, std::string function) {
+void Draw(int num, double a, double b, double e, double minus_border, double border, std::string function) {
     GnuplotPipe gp;
     std::stringstream ss;
+    std::ofstream data("data.dat"); //
+    std::ofstream line("line.dat"); // для отрисовки  x = 0
+    line << 0 << ' ' << minus_border << ' ' << AXIS << '\n';
+    line << 0 << ' ' << border << ' ' << AXIS << '\n';
+    line.close();
+    for (int i = 0; i < num; ++i) {
+        double x = random(a, b);
+        double y = random(minus_border, border);
+        double yf = f(x, function);
+        // if (yf < y) 
+        //     data << x << ' ' << y << ' ' << '0' << '\n';
+        // if (yf > y)
+        //     data << x << ' ' << y << ' ' << '1' << '\n';
+        if (y < yf && 0 < y)
+            data << x << ' ' << y << ' ' << '0' << '\n';
+        if (y > yf && 0 > y)
+            data << x << ' ' << y << ' ' << '1' << '\n';
+        if ((y < yf && 0 > y) || (y > yf && 0 < y))
+            data << x << ' ' << y << ' ' << '2' << '\n';
+    }
+    data.close(); //
     int n = 0;
     while (function.find("^") != -1) { // замена ^ (использует parser) на ** (gnuplot использует)
         n = function.find("^");
@@ -16,34 +38,19 @@ void Draw(int num, double a, double b, double e, double border, std::string func
     }
     while (function.find("e") != -1) { // замена e (использует parser) на 2.7182818284590452 (gnuplot использует)
         n = function.find("e");
-        function.replace(n, 1, "2.7182818284590452");
+        function.replace(n, 1, "2.71828");
     }
-    while (function.find("pi") != -1) { // замена pi (использует parser) на 3.1415926535897932 (gnuplot использует)
-        n = function.find("pi");
-        function.replace(n, 2, "3.1415926535897932");
-    }
-    ss << " set yr [" << 0 << ":" << border << "]\n";
+    ss << " set yr [" << minus_border << ":" << border << "]\n";
     gp.sendLine(ss.str());
     ss.clear();
     ss << " set xr [" << a << ":" << b << "]\n";
     gp.sendLine(ss.str());
     ss.clear();
-    gp.sendLine("set palette defined ( 0 'red', 1 'blue')\n");
-    ss << "plot " << function << ", ";
+    gp.sendLine("set palette model RGB maxcolors 3\n");
+    gp.sendLine("set palette defined ( 0 '#1B8E13', 1 '#EF0000', 2 '#13238E')\n"); // 0: + 1: - 2: остальные
+    ss << "plot 0, (\"line.dat\") with lines lt 1, " << function << ", ";
     ss.clear();
-    std::ofstream data("data.dat");
-    for (int i = 0; i < num; ++i) {
-        double x = random(a, b);
-        double y = random(0, border);
-        double yf = f(x, function);
-        if (yf < y) {
-            data << x << ' ' << y << ' ' << '0' << '\n';
-        } else {
-            data << x << ' ' << y << ' ' << '1' << '\n';
-        }
-    }
-    data.close();
-    ss << "(\"data.dat\") with points palette";
+    ss << "(\"data.dat\") with points pt 7 ps 0.1 palette";
     gp.sendLine(ss.str());
 }
 
